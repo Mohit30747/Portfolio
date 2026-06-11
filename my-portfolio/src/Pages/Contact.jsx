f9d3a60952788173f467f2a0f1c198b92adb32d9
Mohit <mohit@example.com>
2026-06-11 16:14:48 +0530

    fix update backend

--- my-portfolio/src/Pages/Contact.jsx
+++ my-portfolio/src/Pages/Contact.jsx
@@ -1,4 +1,4 @@
-import { motion } from "framer-motion";
+// import { motion } from "framer-motion";
 import { useState } from "react";
 import { FaInstagram, FaGithub, FaWhatsapp ,FaLinkedin } from "react-icons/fa";
 
